08674c2d94f544e6e37167de241962dc581dbdeb
jdandridge86 <jdandridge@eagles.bridgewater.edu>
2025-09-08 09:44:30 -0400

    addUser, setPassword, and getUser

--- a1/database.js
+++ a1/database.js
@@ -1,6 +1,37 @@
+/*class Users {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+}*/
 
+const usersList = [];
 
+function addUser (username) {
+    const userObject = { 
+        username: username,
+        password: "",
+    };
+    //Adds user object to userList with empty password
+    usersList.push(userObject);
+}
 
+function setPassword (username1, password) {
+    //locates object in userList
+    const containsUser = usersList.find(user => user.username === username1);
+    
+    //sets new password if user is located
+    if (containsUser) {
+        containsUser.password = password;
+    }
+}
+
+function getUser (username) {
+    const containsUser = usersList.find(user => user.username === username);
+    if(containsUser) {
+        return containsUser;
+    }
+}
 
 
 
